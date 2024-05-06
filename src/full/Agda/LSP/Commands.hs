4328b13342f42a627a22aa8d42915a7a300fb9be
Jonathan Coates <git@squiddev.cc>
2024-05-06 22:07:53 +0100

    Terrible give support

--- src/full/Agda/LSP/Commands.hs
+++ src/full/Agda/LSP/Commands.hs
@@ -8,37 +8,51 @@ module Agda.LSP.Commands
   ) where
 
 import Data.Text (Text)
-import Data.Aeson (Value, Result(Success), toJSON, fromJSON)
+import Data.Aeson (Value, Result(..), toJSON, fromJSON, ToJSON, FromJSON)
 import qualified Data.Text as Text
 
 import Language.LSP.Protocol.Types as Lsp
 import Agda.Compiler.Backend (primAgdaTCMCommit)
+import GHC.Generics (Generic)
 
 
 -- | The possible commands the LSP server supports
 data AgdaCommand
   = Auto
+  | Give
   deriving (Show, Enum, Bounded)
 
 -- | Get the name of the command that used when converting to a command
 commandName :: AgdaCommand -> Text
 commandName Auto = "auto"
+commandName Give = "give"
 
-data CommandArgs where
+data CommandArgs
   -- | Run Mimer on the interaction point at the given range.
-  Command_Auto :: Uri -> Position -> CommandArgs
+  = Command_Auto Uri Position
+  | Command_Give Uri Position
+  deriving (Generic)
 
 toCommand :: Text -> CommandArgs -> Command
 toCommand title (Command_Auto uri pos) = Command title (commandName Auto) . Just $
   [ toJSON uri
   , toJSON pos
   ]
+toCommand title (Command_Give uri pos) = Command title (commandName Give) . Just $
+  [ toJSON uri
+  , toJSON pos
+  ]
 
 parseCommand :: Text -> [Value] -> Maybe CommandArgs
 parseCommand command args
-  | command == commandName Auto
-  , [uri, pos] <- args
-  , Success uri' <- fromJSON uri
-  , Success pos' <- fromJSON pos
-  = Just (Command_Auto uri' pos')
+  | command == commandName Auto, [uri, pos] <- args = Command_Auto <$> parse uri <*> parse pos
+  | command == commandName Give, [uri, pos] <- args = Command_Give <$> parse uri <*> parse pos
+
   | otherwise = Nothing
+
+  where
+    parse :: FromJSON a => Value -> Maybe a
+    parse = resToMaybe . fromJSON
+
+    resToMaybe (Success x) = Just x
+    resToMaybe (Error _) = Nothing
